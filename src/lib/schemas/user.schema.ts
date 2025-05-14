4af60d1147154f11c33569895acc22bfd00bb409
RustyNeuron <89193350+rustyneuron01@users.noreply.github.com>
2025-05-14 12:20:44 +0200

    Update user.schema.ts

--- src/lib/schemas/user.schema.ts
+++ src/lib/schemas/user.schema.ts
@@ -4,15 +4,9 @@ import { HydratedDocument } from 'mongoose';
 export type UserDocument = HydratedDocument<User>;
 
 @Schema()
-export class User {
+export class UserWallet {
   @Prop({ required: true })
   externalWalletAddress: string;
-
-  @Prop({ required: true })
-  privateKey: string;
-
-  @Prop({ required: true })
-  address: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
